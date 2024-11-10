1d657976e02bf49028b6065d8d3d086e5b3793d1
ASuzuhA <124298064+ASuzuhA@users.noreply.github.com>
2024-11-11 02:01:03 +0800

    Update set_cron_jobs.sh

--- set_cron_jobs.sh
+++ set_cron_jobs.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# Ҫ��ʱִ�еĽű�·��
-CLOUDFLARE="/root/canat/run_cloudflare.sh"
-SOCAT_IP="/root/canat/socat/target_IP.sh"
+# 要定时执行的脚本路径
+CLOUDFLARE="/root/canat.sh"
 
-# ���� cron ����
-CLOUDFLARE_CRON_JOB="0 3 * * * $CLOUDFLARE"  # �賿3��ִ��
-SOCAT_IP_CRON_JOB="10 3 * * * $SOCAT_IP"     # �賿3��10��ִ��
+# 定义 cron 任务
+CLOUDFLARE_CRON_JOB="0 3 * * * $CLOUDFLARE"  # 凌晨3点执行
 
-# ��鵱ǰ�û��� crontab �Ƿ��Ѵ��ڸ�����
+# 检查当前用户的 crontab 是否已存在该任务
 (crontab -l | grep -F "$CLOUDFLARE_CRON_JOB") && {
     echo "CLOUDFLARE CRON JOB already exists."
 } || {
     (crontab -l; echo "$CLOUDFLARE_CRON_JOB") | crontab -
     echo "CLOUDFLARE CRON JOB: $CLOUDFLARE_CRON_JOB"
-}
-
-(crontab -l | grep -F "$SOCAT_IP_CRON_JOB") && {
-    echo "SOCAT_IP CRON JOB already exists."
-} || {
-    (crontab -l; echo "$SOCAT_IP_CRON_JOB") | crontab -
-    echo "SOCAT_IP CRON JOB: $SOCAT_IP_CRON_JOB"
 }
