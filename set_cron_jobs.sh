4a78e147ac3ed995818a9eeaea29b64c92bf6402
ASuzuhA <124298064+ASuzuhA@users.noreply.github.com>
2024-11-11 13:21:32 +0800

    Update set_cron_jobs.sh

--- set_cron_jobs.sh
+++ set_cron_jobs.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# 要定时执行的脚本路径
 CLOUDFLARE="/root/canat.sh"
 
-# 定义 cron 任务
 CLOUDFLARE_CRON_JOB="0 3 * * * $CLOUDFLARE"  # 凌晨3点执行
 
-# 检查当前用户的 crontab 是否已存在该任务
 (crontab -l | grep -F "$CLOUDFLARE_CRON_JOB") && {
     echo "CLOUDFLARE CRON JOB already exists."
 } || {
