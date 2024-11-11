ff602b053708f9d93190630698315f70f5dff579
ASuzuhA <124298064+ASuzuhA@users.noreply.github.com>
2024-11-11 23:04:33 +0800

    Update socat.sh

--- socat/socat.sh
+++ socat/socat.sh
@@ -5,4 +5,4 @@ TARGET_IP=104.18.146.197
 TARGET_PORT=8080
 
 # 启动 socat
-socat TCP-LISTEN:$LOCAL_PORT,fork TCP:$TARGET_IP:$TARGET_PORT
+socat TCP-LISTEN:$LOCAL_PORT,fork TCP:$TARGET_IP:$TARGET_PORT 2>> /root/canat/self_test/error.log
