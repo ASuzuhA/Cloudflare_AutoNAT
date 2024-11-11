5763d017ea1ab303ec8df545eded7992e7365f2e
ASuzuhA <124298064+ASuzuhA@users.noreply.github.com>
2024-11-11 13:22:07 +0800

    Update socat.sh

--- socat/socat.sh
+++ socat/socat.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# socat 转发脚本
-# 将本地端口 9876 转发到目标 IP（Cloudflare CDN 的 IP）和端口
-
 LOCAL_PORT=7896
 TARGET_IP=104.18.146.197
 TARGET_PORT=8080
 
-# 启动 socat
+# 鍚姩 socat
 socat TCP-LISTEN:$LOCAL_PORT,fork TCP:$TARGET_IP:$TARGET_PORT
